4b9328e58c632de10575fabed2e47afe762d5e98
KennethKcai <97841755+KennethKcai@users.noreply.github.com>
2024-02-19 17:38:18 -0800

    img

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.getElementById('homeLink').addEventListener('click', function(event){
             var imagePadding = link.getAttribute('data-padding');
             var imagePreview = document.getElementById('image-preview');
 
+            link.style.color = 'black';
             imagePreview.style.backgroundImage = 'url(' + imageUrl + ')';
             imagePreview.style.padding = imagePadding;
             imagePreview.style.display = 'block';
@@ -18,5 +19,8 @@ document.getElementById('homeLink').addEventListener('click', function(event){
         item.addEventListener('mouseleave', function(){
             var imagePreview = document.getElementById('image-preview');
             imagePreview.style.display = 'none';
+
+            var link = item.querySelector('.text-link');
+            link.style.color = '';
         });
     });
